0f080292a3c0b37b3018945bc84d1f101e352b1d
HLB288 <henrilb.hlb@gmail.com>
2025-08-04 17:33:16 +0200

    Spring 3 - Task 1

--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,9 +1,16 @@
-use crate::helper::TestApp;
+use crate::helper::{get_random_email, TestApp};
 
+
+// #[tokio::test]
+// async fn login_returns_success() {
+//     let app = TestApp::new().await;
+//     let response = app.post_login().await;
+//     assert_eq!(response.status().as_u16(), 200);
+// }
 
 #[tokio::test]
-async fn login_returns_success() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
     let response = app.post_login().await;
-    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.status().as_u16(), 422);
 }
